104e37869970c8f79fa837f844c118cedb7adbef
Istalantar <istalantar@gmail.com>
2023-09-02 10:50:57 +0200

    update dataclass

--- app/Tile.py
+++ app/Tile.py
@@ -5,5 +5,11 @@ from PIL import Image
 
 @dataclass
 class Tile:
+    """Represents one tile of a whole picture"""
+
     image: Image.Image
+    """Tile as PIL.Image object"""
+    position: tuple
+    """The coordinates (x, y) of the tile in the original picture"""
     rotation: int = 0
+    """The rotation of the tile in degrees"""
