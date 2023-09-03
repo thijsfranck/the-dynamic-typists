4b915b7e3b2b5502baddcdfc2ded4ddf000f4199
Istalantar <istalantar@gmail.com>
2023-09-03 08:48:07 +0200

    update templates

--- app/picture.py
+++ app/picture.py
@@ -1,22 +1,28 @@
-"""Picture module."""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from PIL import Image
+
+if TYPE_CHECKING:
+    from tile import Tile
 
 
 class Picture:
     """Picture class."""
 
-    def __init__(self) -> None:
-        self.image = None
-        self.is_solved = False
-        self.tiles = {}
-        self.tile_order = []
+    def __init__(self, img_path: str) -> None:
+        self.image: Image.Image = Image.open(img_path)
+        self.is_solved: bool = False
+        self.tiles: dict[int, Tile] = {}
+        self.tile_order: list[int] = []
 
-    def load(self, img_path: str) -> None:
-        """Load an image from a file path.
-
-        :param img_path: Absolute path to the image file
-        """
-        self.image = Image.open(img_path)
+    def save(self, img_path: str) -> None:
+        """Save current tile arrangement as file."""
+        new_image = Image.new(mode=self.image.mode, size=self.image.size)
+        for original_pos, new_pos in enumerate(self.tile_order):
+            new_image.paste(self.tiles[new_pos].image, self.tiles[original_pos].position)
+        new_image.save(img_path)
 
     def is_image_fixed(self) -> None:
         """Check if the scrambled tiles are put back to the original."""
--- app/scrambler.py
+++ app/scrambler.py
@@ -1,18 +1,17 @@
-"""Scrambler module."""
 from picture import Picture
 
 
 class Scrambler:
-    """Scrambler class."""
+    """Scramble a picture in different ways."""
 
-    def scramble_rows(self, picture: Picture) -> Picture:
+    def scramble_rows(self, picture: Picture) -> None:
         """Split an Image up into rows an rearranges them."""
         raise NotImplementedError
 
-    def scramble_grid(self, picture: Picture) -> Picture:
+    def scramble_grid(self, picture: Picture) -> None:
         """Split an Image up into tiles an rearranges them."""
         raise NotImplementedError
 
-    def scramble_circle(self, picture: Picture) -> Picture:
+    def scramble_circle(self, picture: Picture) -> None:
         """Split an Image up into circular tiles an rearranges them."""
         raise NotImplementedError
