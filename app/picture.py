3b809366d0176ffbbd72a8addaa3486793113125
Mayur Odedara <maxyodedara5@gmail.com>
2023-09-04 20:51:24 +0530

    Tile scrambler (#8)
    Co-authored-by: Istalantar <istalantar@gmail.com>

--- app/picture.py
+++ app/picture.py
@@ -1,3 +1,4 @@
+"""Picture class contains template for picture based on Image from Pillow."""
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
@@ -9,19 +10,25 @@ if TYPE_CHECKING:
 
 
 class Picture:
-    """Picture class."""
+    """Picture class has methods for saving PIL image formed from tiles.
+
+    :param img_path: Used to provide image path to open image.
+    """
 
     def __init__(self, img_path: str) -> None:
         self.image: Image.Image = Image.open(img_path)
         self.is_solved: bool = False
         self.tiles: dict[int, Tile] = {}
         self.tile_order: list[int] = []
+        self.scramble_type = None
 
     def save(self, img_path: str) -> None:
         """Save current tile arrangement as file."""
         new_image = Image.new(mode=self.image.mode, size=self.image.size)
+
         for original_pos, new_pos in enumerate(self.tile_order):
             new_image.paste(self.tiles[new_pos].image, self.tiles[original_pos].position)
+
         new_image.save(img_path)
 
     def is_image_fixed(self) -> None:
--- app/scrambler.py
+++ app/scrambler.py
@@ -1,30 +1,72 @@
-from random import shuffle
+"""Scramble has functions for scrambling images in different ways."""
+from random import choice, shuffle
 
 from picture import Picture
 from tile import Tile
 
 
-class Scrambler:
-    """Scrambler class."""
+def scramble_rows(picture: Picture) -> None:
+    """Split an Image up into rows an rearranges them."""
+    num_tiles = 8
+    image_size = picture.image.size
+    tile_size = (image_size[0], int(image_size[1] / num_tiles))
 
-    @classmethod
-    def scramble_rows(cls, picture: Picture) -> None:
-        """Split an Image up into rows an rearranges them."""
-        num_tiles = 8
-        image_size = picture.image.size
-        tile_size = (image_size[0], int(image_size[1] / num_tiles))
+    for y in range(num_tiles):
+        co_ordinate = (0, tile_size[1] * y, tile_size[0], tile_size[1] * (y + 1))
+        picture.tiles[y] = Tile(picture.image.crop(co_ordinate), (co_ordinate[0], co_ordinate[1]))
 
-        for y in range(num_tiles):
-            x0, y0, x1, y1 = (0, tile_size[1] * y, tile_size[0], tile_size[1] * (y + 1))
-            picture.tiles[y] = Tile(picture.image.crop((x0, y0, x1, y1)), (x0, y0))
+    picture.tile_order = list(picture.tiles.keys())
+    shuffle(picture.tile_order)
 
-        picture.tile_order = list(picture.tiles.keys())
-        shuffle(picture.tile_order)
 
-    def scramble_grid(self, picture: Picture) -> Picture:
-        """Split an Image up into tiles an rearranges them."""
-        raise NotImplementedError
+def scramble_grid(picture: Picture, num_of_tiles: int = 4) -> None:
+    """Split an Image up into tiles an rearranges them and rotates each tile randomly."""
+    co_ordinates = [
+        (0, 0, 512, 512),
+        (512, 0, 1024, 512),
+        (0, 512, 512, 1024),
+        (512, 512, 1024, 1024),
+    ]
 
-    def scramble_circle(self, picture: Picture) -> Picture:
-        """Split an Image up into circular tiles an rearranges them."""
-        raise NotImplementedError
+    # Tile swap logic
+    tile_order = [0, 1, 2, 3]
+    swap_list = ["top", "left", "bottom", "right"]
+    tile_rotations = [choice([90, 180, 270]) for _ in range(num_of_tiles)]
+    _swap_order = []
+
+    shuffle(swap_list)
+
+    # Swap order can be used to keep track of swaps
+    _swap_order = swap_list
+
+    for current_choice in swap_list:
+        if current_choice == "top":
+            tile_order[0], tile_order[1] = tile_order[1], tile_order[0]
+
+        if current_choice == "left":
+            tile_order[0], tile_order[2] = tile_order[2], tile_order[0]
+
+        if current_choice == "bottom":
+            tile_order[2], tile_order[3] = tile_order[3], tile_order[2]
+
+        if current_choice == "right":
+            tile_order[1], tile_order[3] = tile_order[3], tile_order[1]
+
+    # Order of tiles to be formed
+    picture.tile_order = tile_order
+
+    # Adding tiles for picture
+    for tile in range(num_of_tiles):
+        tile_img = picture.image.crop(co_ordinates[tile])
+        tile_img = tile_img.rotate(tile_rotations[tile])
+        picture.tiles[tile] = Tile(
+            tile_img,
+            (co_ordinates[tile][0], co_ordinates[tile][1]),
+        )
+
+
+def scramble_circle(picture: Picture) -> Picture:
+    """Split an Image up into rows an rearranges them."""
+    # Added line for linter escape
+    _ = picture
+    raise NotImplementedError
