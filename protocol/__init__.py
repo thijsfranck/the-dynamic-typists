abdf594f47ca684916b58335b46bcda117200861
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-09-10 17:32:08 +0100

    Convert protocol.__all__ to a tuple (#28)

--- protocol/__init__.py
+++ protocol/__init__.py
@@ -6,9 +6,9 @@ These classes are designed for use by both frontend and backend applications.
 from .solution import Solution, SolutionRequest, SolutionResponse
 from .tiles import TilesResponse
 
-__all__ = [
+__all__ = (
     "Solution",
     "SolutionRequest",
     "SolutionResponse",
     "TilesResponse",
-]
+)
